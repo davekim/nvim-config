e36a65135385d4d2ab89343d2d93c939f6462d7f
davekim <davekim@users.noreply.github.com>
2025-02-15 01:07:37 -0600

    Customize rose-pine palette for more muted feel

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,19 @@ return {
     lazy = false,
     name = "rose-pine",
     config = function()
+        require("rose-pine").setup({
+            variant = "main",
+            dark_variant = "main",
+            palette = {
+                main = {
+                    base = "#1e1e1e",
+                    pine = "#7f9f9f",
+                    gold = "#f9bd98",
+                    foam = "#bedfe0",
+                    iris = "#cf637e",
+                },
+            },
+        })
         vim.cmd("colorscheme rose-pine")
     end
 }
