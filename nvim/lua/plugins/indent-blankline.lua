936632af7d9f2353eba728bd1d499f9eb9ddd12b
davekim <davekim@users.noreply.github.com>
2025-02-16 12:27:59 -0600

    Add indent-blankline plugin, minimal with scope disabled for now

---
+++ nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,17 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {
+        indent = {
+            char = '▏',
+        },
+        scope = {
+            enabled = false,
+            char = '▏',
+            show_start = false,
+            show_end = false,
+        }
+    },
+}
