6d2d921cfe3f91571b183b1eb190b13315ee26a2
davekim <davekim@users.noreply.github.com>
2025-02-16 16:48:30 -0600

    Add ruby and python lsp, format vim keymap

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ts_ls" }
+                ensure_installed = { "lua_ls", "ts_ls", "ruby_lsp", "pyright" }
             })
         end
     },
@@ -25,10 +25,17 @@ return {
             lspconfig.ts_ls.setup({
                 capabilities = capabilities
             })
+            lspconfig.ruby_lsp.setup({
+                capabilities = capabilities
+            })
+            lspconfig.pyright.setup({
+                capabilities = capabilities
+            })
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
             vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
         end,
     }
