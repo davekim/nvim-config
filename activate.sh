d1e937821debc6f6d4dd5f3960071d195ba60599
davekim <davekim@users.noreply.github.com>
2025-07-30 20:47:39 -0500

    Modify removing existing link

--- activate.sh
+++ activate.sh
@@ -6,7 +6,6 @@ dir=$(cd "$(dirname "$0")"; pwd)
 mkdir -p "${HOME}/.config/nvim"
 
 for name in lua init.lua lazy-lock.json; do
-    # TODO: Make this safer
-    rm -rf "${HOME}/.config/nvim/${name}"
+    rm "${HOME}/.config/nvim/${name}"
     ln -s "${dir}/nvim/${name}" "${HOME}/.config/nvim/${name}"
 done
